2b50a573a4f1d43089624cd6aace0ef489765856
Priska Kohnen <priskakohnen@gmail.com>
2020-12-07 14:24:30 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
+import React from 'react';
+import PropTypes from 'prop-types';
+import ReactDom from 'react-dom';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class Rectangle extends React.Component{
+  render(){
+      let name = this.props.name
+      return (
+            <div className="Rectangle" style={{width: 300+'px', height: 50+'px', border: 1+'px solid black'}}>
+                {name}
+            </div>
+      );
+  }
+  static defaultProps = {
+      name: '/',
+      type: 'directory'
+  }
+  static propTypes = {
+      name: PropTypes.string.isRequired,
+      visible: PropTypes.bool,
+      type: PropTypes.string.isRequired,
+  }
 }
 
-export default App;
+class Methods extends React.Component{
+    add(){
+        return ReactDom.render(<Rectangle/>, document.getElementById('a'))
+    }
+    delete(){
+        ReactDom.unmountComponentAtNode(document.getElementById('a'))
+    }
+    move(){
+        return ReactDom.render(<Rectangle/>, document.getElementById('a'))
+    }
+    link(){
+        ReactDom.unmountComponentAtNode(document.getElementById('a'))
+    }
+    render(){
+        return (
+            <div>
+                <button onClick={this.add.bind(this)}>Add</button>
+                <button onClick={this.delete.bind(this)}>Delete</button>
+                <button onClick={this.move.bind(this)}>Move</button>
+                <button onClick={this.link.bind(this)}>Link</button>
+                <div id="a"></div>
+            </div>
+            )
+    }
+}
+export default Methods;
